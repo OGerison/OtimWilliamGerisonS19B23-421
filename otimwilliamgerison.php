19ec1a60e74238a236cebae7e5e3df716a193a05
OGerison <56107729+OGerison@users.noreply.github.com>
2020-12-18 09:30:00 +0300

    Add files via upload

---
+++ otimwilliamgerison.php
@@ -0,0 +1,66 @@
+<!DOCTYPE>
+<html>
+<head>
+    <title>ABOUT</title>
+    <link rel="stylesheet" href="OTIM WILLIAM GERISON.css">
+    <meta charset = "utf-8">
+</head>
+<body>
+<div class="header">
+	<div class="header-logo">
+		<img src="ortis.jpg"height = "50px" width = "50px">
+	</div>
+	<h1>OTIM WILLIAM GERISON</h1>
+</div>
+<h2>S19B23/421</h2>
+<h2>BSCS 2</h2>
+<hr width = "100%">
+<h2>COURSE EXPECTATIONS</h2>
+<h3>To understand web development on a greater scale and use the knowledge in future projects.</h3>
+<h2>LANGUAGES KNOWN.</h2>
+<b>
+<ol>
+	<li>C++</li>
+	<li>Java</li>
+	<li>HTML & CSS</li>
+	<li>PHP</li>
+	</ol>
+</b>
+<h2>HOBBIES</h2>
+<b>
+<ul>
+	<li>Watching and analyzing football.</li>
+<video width="320" height="240" controls>
+  <source src="aaron.mp4" type="video/mp4">
+</video>
+	<li>Watching Documentaries.</li>
+	<li>Travelling.</li>
+</ul>
+</b>
+<b>
+<p>Type Your Feedback Down Below.
+<a href="https://moodle.ucu.ac.ug/login/index.php">
+<img border="0" alt="ortis" src="ortis.jpg" width="200px" height="200px"></a>
+</p>
+</b>
+</div>
+<div>
+<form action ="bio.php" method = "POST">
+	Name
+	<input type="text" name = "name"/>
+	<br>
+	Email
+	<input type="text" name = "emails"/>
+	<br>
+	Telephone 
+	<input type="text" name = "telephone"/>
+	<br>
+	Comment
+	<input type="text" name = "comment"/>
+	<br>
+	<input type="submit" name = "SAVE"  value = "INSERT" />
+	<input type="submit" name = "SAVED"  value = "DISPLAY" />
+	</form>	
+</div>
+</body>
+</html>
